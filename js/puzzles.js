92d14a4b8466285cb3fe5400b0e8beb1987368f5
patbodem-source <patbodem@gmail.com>
2026-03-23 11:23:15 +0000

    Add files via upload

---
+++ js/puzzles.js
@@ -0,0 +1,11 @@
+// js/puzzles.js
+const PUZZLES = [
+    // Famous tactical positions
+    "r1b1k2r/pppp1ppp/2n2n2/8/1bB1P3/2N2N2/PPP2PPP/R1B1K2R w KQkq - 5 8", // simple developing tactic
+    "r1bqk2r/pp1n1ppp/2pbpn2/3p4/2PP4/2N1PN2/PP1B1PPP/R2QKB1R w KQkq - 3 7", // Queen's Gambit Declined
+    "r2q1rk1/1pp2ppp/p1np1n2/4p3/1b2P1b1/2NP1N2/PPP1BPPP/R1BQ1RK1 b - - 1 9", // Ruy Lopez position
+    "rnbq1k1r/pp1pbppp/2p2n2/8/2B1P3/2N2N2/PPPP1PPP/R1BQK2R w KQ - 4 7", // Scotch Game
+    "6k1/1p3ppp/p1p5/8/8/2P5/PP3PPP/4R1K1 w - - 0 24", // Back rank mate situation
+    "rnbqk2r/ppp1bppp/4pn2/3p4/2PP4/2N2N2/PP2PPPP/R1BQKB1R w KQkq - 4 5", // QGD Exchange variation
+    "8/8/8/6K1/8/8/R7/6k1 w - - 0 1" // simple rook and king mate for practice
+];
